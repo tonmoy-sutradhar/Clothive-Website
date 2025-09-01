b2d3bca5404031803a9aa256244fa196ca5cbe33
Tonmoy <tonmoysutradhar2002@gmail.com>
2025-09-02 00:54:49 +0600

    Admin profile done.

--- admin/src/routers/Router.jsx
+++ admin/src/routers/Router.jsx
@@ -4,17 +4,24 @@ import Add from "../pages/Add";
 import Lists from "../pages/Lists";
 import Orders from "../pages/Orders";
 import Login from "../pages/Login";
+import { useContext } from "react";
+import { adminDataContext } from "../Context/AdminContext";
 
 function Router() {
+  let { adminData } = useContext(adminDataContext);
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home></Home>}></Route>
-        <Route path="/add" element={<Add></Add>}></Route>
-        <Route path="/lists" element={<Lists></Lists>}></Route>
-        <Route path="/orders" element={<Orders></Orders>}></Route>
-        <Route path="/login" element={<Login></Login>}></Route>
-      </Routes>
+      {!adminData ? (
+        <Login></Login>
+      ) : (
+        <Routes>
+          <Route path="/" element={<Home></Home>}></Route>
+          <Route path="/add" element={<Add></Add>}></Route>
+          <Route path="/lists" element={<Lists></Lists>}></Route>
+          <Route path="/orders" element={<Orders></Orders>}></Route>
+          <Route path="/login" element={<Login></Login>}></Route>
+        </Routes>
+      )}
     </>
   );
 }
