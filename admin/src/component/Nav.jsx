d127463ba19ae92e64dbf724884b64c3d322466b
Tonmoy <tonmoysutradhar2002@gmail.com>
2025-09-17 15:57:30 +0600

    Admin logout functionality implement done.

--- admin/src/component/Nav.jsx
+++ admin/src/component/Nav.jsx
@@ -1,8 +1,29 @@
 import { useNavigate } from "react-router-dom";
 import logo from "../assets/logo.png";
+import axios from "axios";
+import { useContext } from "react";
+import { adminDataContext } from "./../Context/AdminContext";
 
 function Nav() {
   let navigate = useNavigate();
+  let { getAdmin } = useContext(adminDataContext);
+
+  const logOut = async () => {
+    try {
+      const result = await axios.get(
+        `${import.meta.env.VITE_API_URL}/api/auth/logout`,
+        {
+          withCredentials: true,
+        }
+      );
+      console.log(result.data);
+      getAdmin();
+      navigate("/login");
+    } catch (err) {
+      console.log(err, "Admin logout issue");
+    }
+  };
+
   return (
     <div className="w-[100vw] h-[70px] bg-[#dcdbdbf8] z-10 fixed top-0 flex items-center justify-between px-[30px] overflow-x-hidden shadow-md shadow-black">
       <div
@@ -11,11 +32,13 @@ function Nav() {
       >
         <img src={logo} alt="Logo" className="w-[30px]" />
         <h1 className="text-[25px] text-[black] font-sans">CLOTHIVE</h1>
-
-        <button className="text-[15px] hover:border-[2px] border-[#89daea] cursor-pointer bg-[#000000ca] py-[10px] px-[20px] rounded-2xl text-white">
-          LogOut
-        </button>
       </div>
+      <button
+        onClick={logOut}
+        className="text-[15px] hover:border-[2px] border-[#89daea] cursor-pointer bg-[#000000ca] py-[10px] px-[20px] rounded-2xl text-white"
+      >
+        LogOut
+      </button>
     </div>
   );
 }
