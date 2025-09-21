6411a8109d4dc85502779410ca0f3429ace76342
Tonmoy <tonmoysutradhar2002@gmail.com>
2025-09-21 22:45:46 +0600

    Cloudinary setup done.

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -1,3 +1,27 @@
 import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
 
-const uploadOnCloudinary = async (filePath) => {};
+const uploadOnCloudinary = async (filePath) => {
+  // Configuration
+  cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET,
+  });
+
+  try {
+    if (!filePath) {
+      return null;
+    }
+
+    // Upload an image
+    const uploadResult = await cloudinary.uploader.upload(filePath);
+    fs.unlinkSync(filePath);
+    return uploadResult.secure_url;
+  } catch (err) {
+    fs.unlinkSync(filePath);
+    console.log(err, "Cloudinary issue.");
+  }
+};
+
+export default uploadOnCloudinary;
